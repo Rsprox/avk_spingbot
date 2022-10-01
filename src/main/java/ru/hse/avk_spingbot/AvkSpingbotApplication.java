122413453e40d6246c298b6959a4a897a7f9314c
Rsprox <11vsrsx11@gmail.com>
2022-10-01 12:40:03 +0500

    Initial commit

---
+++ src/main/java/ru/hse/avk_spingbot/AvkSpingbotApplication.java
@@ -0,0 +1,13 @@
+package ru.hse.avk_spingbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvkSpingbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvkSpingbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hse/avk_spingbot/AvkSpingbotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.avk_spingbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvkSpingbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
